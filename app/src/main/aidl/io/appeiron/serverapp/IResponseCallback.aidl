f6bd8f1ecb4cc49200d64ef653c8f5dd0faf929b
katharmoi <kadirkertis@gmail.com>
2022-06-01 12:37:38 +0300

    Implement Service

--- app/src/main/aidl/io/appeiron/serverapp/IResponseCallback.aidl
+++ app/src/main/aidl/io/appeiron/serverapp/IResponseCallback.aidl
@@ -4,7 +4,6 @@ package io.appeiron.serverapp;
 // Interface for the response callback
 
 interface IResponseCallback {
-
     void onSuccess(String msg);
     void onFailure(String msg);
 }
--- app/src/main/java/io/appeiron/serverapp/service/ServerService.kt
+++ app/src/main/java/io/appeiron/serverapp/service/ServerService.kt
@@ -3,16 +3,35 @@ package io.appeiron.serverapp.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.os.Process
+import android.os.RemoteException
+import android.util.Log
+import io.appeiron.serverapp.IResponseCallback
+import io.appeiron.serverapp.ISampleApi
 
-
+// Sample Server class
 class ServerService : Service() {
 
-//    private val binder = object : ISampleApi.Stub() {
-//
-//
-//    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    override fun onBind(intent: Intent?): IBinder {
+        return ServerBinder()
     }
+
+    inner class ServerBinder : ISampleApi.Stub() {
+        override fun respond(cb: IResponseCallback?) {
+            ServerThread(cb).start()
+        }
+    }
+
+    inner class ServerThread(private val cb: IResponseCallback?) : Thread() {
+        override fun run() {
+            sleep(5000)
+            try {
+                cb?.onSuccess(Process.myPid().toString())
+            } catch (e: RemoteException) {
+                Log.e("SampleJob", "Exception when calling onSuccess()", e)
+            }
+
+        }
+    }
+
 }

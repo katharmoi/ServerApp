88ae1161459c2f31b6af845304da8ef90b38cba9
katharmoi <kadirkertis@yahoo.com.tr>
2022-06-09 09:13:35 +0300

    Implemet LocalBroadcast for  messaging between Activity and Service

--- app/src/main/java/io/appeiron/serverapp/service/ServerService.kt
+++ app/src/main/java/io/appeiron/serverapp/service/ServerService.kt
@@ -6,8 +6,12 @@ import android.os.IBinder
 import android.os.Process
 import android.os.RemoteException
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.appeiron.serverapp.IResponseCallback
 import io.appeiron.serverapp.ISampleApi
+
+const val SERVER_MSG_INTENT = "server_msg_intent"
+const val SERVER_MSG_KEY = "request_id"
 
 // Sample Server class
 class ServerService : Service() {
@@ -17,21 +21,35 @@ class ServerService : Service() {
     }
 
     inner class ServerBinder : ISampleApi.Stub() {
-        override fun respond(cb: IResponseCallback?) {
-            ServerThread(cb).start()
+
+        override fun getResponse(requestId: String?, cb: IResponseCallback?) {
+            ServerThread(requestId, cb).start()
         }
     }
 
-    inner class ServerThread(private val cb: IResponseCallback?) : Thread() {
+    inner class ServerThread(private val requestId: String?, private val cb: IResponseCallback?) :
+        Thread() {
         override fun run() {
             sleep(5000)
             try {
-                cb?.onSuccess(Process.myPid().toString())
+                cb?.onSuccess(requestId, Process.myPid().toString())
+                sendMessage(requestId)
             } catch (e: RemoteException) {
                 Log.e("SampleJob", "Exception when calling onSuccess()", e)
             }
 
         }
+    }
+
+    /**
+     * Sends LocalBroadcast msg for activity
+     */
+    private fun sendMessage(msg: String?) {
+        val intent = Intent(SERVER_MSG_INTENT).apply {
+            putExtra(SERVER_MSG_KEY, msg)
+        }
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
 
 }

6aa1b2f9aa731322db033dc9e0fd8b300c0addf9
katharmoi <kadirkertis@yahoo.com.tr>
2022-06-09 09:14:12 +0300

    Update Activity to listen for broadcast

--- app/src/main/java/io/appeiron/serverapp/domain/model/Client.kt
+++ app/src/main/java/io/appeiron/serverapp/domain/model/Client.kt
@@ -1,7 +1,5 @@
 package io.appeiron.serverapp.domain.model
 
 data class Client(
-    val clientPackageName: String,
-    val clientProcessId:String,
-    var clientData:String
+    var clientRequestId:String
 )
--- app/src/main/java/io/appeiron/serverapp/presentation/MainActivity.kt
+++ app/src/main/java/io/appeiron/serverapp/presentation/MainActivity.kt
@@ -1,26 +1,52 @@
 package io.appeiron.serverapp.presentation
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import io.appeiron.serverapp.databinding.ActivityMainBinding
-import io.appeiron.serverapp.domain.model.LatestClient
+import io.appeiron.serverapp.service.SERVER_MSG_INTENT
+import io.appeiron.serverapp.service.SERVER_MSG_KEY
+
 
 class MainActivity : AppCompatActivity() {
 
     //Main Layout Binding
     private lateinit var binding: ActivityMainBinding
 
+    //Receiver for the service broadcasts
+    lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Initialize Receiver
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(ctx: Context?, intent: Intent?) {
+                val message = intent?.getStringExtra(SERVER_MSG_KEY)
+                binding.txtRequestId.text = message
+            }
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val client = LatestClient.client
+    override fun onResume() {
+        super.onResume()
 
-        binding.txtPackage.text = client?.clientPackageName
-        binding.txtPid.text = client?.clientProcessId
+        //Register Receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            receiver,
+            IntentFilter(SERVER_MSG_INTENT)
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //Unregister the receiver
+        unregisterReceiver(receiver)
     }
 }
